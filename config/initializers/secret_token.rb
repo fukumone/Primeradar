a4d61af1fcb9d5834fbb841a9ffbff51bfef14c6
t-fukui <fukui@rubyquitous.co.jp>
2013-11-30 11:57:51 +0900

    Hiyiyi start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hiyiyi::Application.config.secret_key_base = 'aa102f28e8cf43a145ceb6f116eb137dc18e371d71cfc889bcc3fece0ac5a145186f43e436991d9e85b50062c28289ebe41b9f3cda0c8808022bd92069272781'
