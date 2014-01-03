3646424f9d6df77aa07ea0bb0e473e165af92c78
t-fukui <fukui@rubyquitous.co.jp>
2014-01-03 13:56:23 +0900

    検索機能

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,12 @@
 class TopController < ApplicationController
+
   def index
+    if params[:prime_number].present?
+      @prime = params[:prime_number].to_i
+    end
   end
+
+  def search
+  end
+
 end
--- app/helpers/prime_helper.rb
+++ app/helpers/prime_helper.rb
@@ -1,19 +1,18 @@
 module PrimeHelper
 
-def prime_number(prime)
-  arr = []
-  (1..prime).each do |num|
-    if prime % num == 0
-      arr << num
+  def prime_number(prime)
+    arr = []
+    (1..prime).each do |num|
+      if prime % num == 0
+        arr << num
+      end
+    end
+
+    if arr.count < 3 && prime != 1 && 0 < prime
+      "#{prime}は素数です"
+    else
+      "#{prime}は素数ではない"
     end
   end
-
-  if arr.count < 3 && prime != 1 && 0 < prime
-    "#{prime}は素数です"
-  else
-    "#{prime}は素数ではない"
-  end
-end
-
 
 end
