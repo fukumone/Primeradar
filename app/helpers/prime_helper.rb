7794deff5c37575d3e927272ca4b23968758701e
t-fukui <fukui@rubyquitous.co.jp>
2014-01-03 21:39:14 +0900

    修正

--- app/helpers/prime_helper.rb
+++ app/helpers/prime_helper.rb
@@ -3,10 +3,16 @@ module PrimeHelper
   def prime_number(number)
     sqrt = Math.sqrt(number)
     factor_found = (2..sqrt).any? { |i| number % i == 0 }
+    unless factor_found || number == 1
+      number_with_delimiter(number)
+    end
+  end
+
+  def composite_number(number)
+    sqrt = Math.sqrt(number)
+    factor_found = (2..sqrt).any? { |i| number % i == 0 }
     if factor_found || number == 1
-      "#{number_with_delimiter(number)}は素数ではない\n"
-    else
-      "#{number_with_delimiter(number)}は素数である\n"
+      number_with_delimiter(number)
     end
   end
 
