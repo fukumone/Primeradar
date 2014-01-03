e474e10a1c52e2bb1812731c0e57496c63447078
t-fukui <fukui@rubyquitous.co.jp>
2014-01-03 23:08:22 +0900

    範囲検索機能追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
+  before_action :range_number
 
   def index
     if params[:prime_number].present?
@@ -9,4 +10,15 @@ class TopController < ApplicationController
   def search
   end
 
+  private
+  def range_number
+    @prime_number_from = 1
+    @prime_number_to = 50
+    if params[:prime_number_to].present?
+      @prime_number_to = params[:prime_number_to].to_i
+    end
+    if params[:prime_number_from].present?
+      @prime_number_from = params[:prime_number_from].to_i
+    end
+  end
 end
