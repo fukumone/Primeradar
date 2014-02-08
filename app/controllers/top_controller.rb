c48d356a592a3f0bfb74da0679b513afb904e87d
t-fukui <fukui@rubyquitous.co.jp>
2014-02-08 13:12:25 +0900

    error機能追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,48 +3,38 @@ class TopController < ApplicationController
   # before_action :range_number
   # before_action :twins_range_number
 
-  # include ErrorsHelper
-
   def index
-
-  end
-
-  def search
-    if params[:prime_number].present?
-      @prime = params[:prime_number].to_i
+    @search_form = PrimeNumberSearchForm.new(params)
+    if @search_form.valid?
+      if params[:prime_number].present?
+        @prime = params[:prime_number].to_i
+      end
+    else
+      flash.now.alert  = @search_form.errors.full_messages.join('<br />').html_safe
       render "index"
     end
   end
 
   def search_prime
-    @search_range = PrimeNumber.search_range_prime(params)
-    render "index"
+    @search_form = PrimeNumberSearchForm.new(params)
+    if @search_form.valid?
+      @search_range = PrimeNumber.search_range_prime(params)
+      render "index"
+    else
+      flash.now.alert  = @search_form.errors.full_messages.join('<br />').html_safe
+      render "index"
+    end
   end
 
   def search_twins_prime
-    @search_twins_range = PrimeNumber.search_twins_range_prime(params)
-    render "index"
+    @search_form = PrimeNumberSearchForm.new(params)
+    if @search_form.valid?
+      @search_twins_range = PrimeNumber.search_twins_range_prime(params)
+      render "index"
+    else
+      flash.now.alert  = @search_form.errors.full_messages.join('<br />').html_safe
+      render "index"
+    end
   end
-
-  # private
-  # def range_number
-  #   @prime_number_from = 1
-  #   if params[:prime_number_to].present?
-  #     @prime_number_to = params[:prime_number_to].to_i
-  #   end
-  #   if params[:prime_number_from].present?
-  #     @prime_number_from = params[:prime_number_from].to_i
-  #   end
-  # end
-
-  # def twins_range_number
-  #   @twins_prime_number_from = 0
-  #   if params[:twins_prime_number_to].present?
-  #     @twins_prime_number_to = params[:twins_prime_number_to].to_i
-  #   end
-  #   if params[:twins_prime_number_from].present?
-  #     @twins_prime_number_from = params[:twins_prime_number_from].to_i
-  #   end
-  # end
 
 end
